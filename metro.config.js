b5b12530c772f1b71e1cf538c42dfc8ffa6a33e6
Dotman <dotvex@dev.local>
2026-08-22 23:34:00 +0100

    mobile-app branch: Expo/React Native DOTVEX app ready for CodeMagic

---
+++ metro.config.js
@@ -0,0 +1,27 @@
+const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
+
+const config = getDefaultConfig(__dirname);
+
+config.resolver.unstable_enablePackageExports = true;
+
+config.resolver.nodeModulesPaths = [
+  path.resolve(__dirname, 'node_modules'),
+  path.resolve(__dirname, '../node_modules'),
+];
+
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (moduleName === '@dotvex/shared') {
+    return {
+      filePath: path.resolve(__dirname, '../shared/index.ts'),
+      type: 'sourceFile',
+    };
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
+
+config.watchFolders = [
+  path.resolve(__dirname, '../shared'),
+];
+
+module.exports = config;
